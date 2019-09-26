c58a978a993694990185f83b353285ced324343d
Duc Pham <ducpham@kms-technology.com>
2019-09-26 18:07:36 +0700

    [DOCGEN] sample

--- @common/elastic/README.md
+++ @common/elastic/README.md
@@ -31,6 +31,127 @@
 ## Actions
 
 <!-- AUTO-CONTENT-START:ACTIONS -->
+## `bulk` 
+
+BULK
+
+### Parameters
+| Property | Type | Default | Description |
+| -------- | ---- | ------- | ----------- |
+| `ctx` | `Context.<RequestParams.Bulk>` | **required** |  |
+
+### Results
+**Type:** any
+
+any
+
+
+## `create` 
+
+CREATE
+
+### Parameters
+| Property | Type | Default | Description |
+| -------- | ---- | ------- | ----------- |
+| `ctx` | `Context.<RequestParams.Create>` | **required** |  |
+
+### Results
+**Type:** any
+
+any
+
+
+## `get` 
+
+GET
+
+### Parameters
+| Property | Type | Default | Description |
+| -------- | ---- | ------- | ----------- |
+| `ctx` | `Context.<RequestParams.Get>` | **required** |  |
+
+### Results
+**Type:** any
+
+any
+
+
+## `update` 
+
+UPDATE
+
+### Parameters
+| Property | Type | Default | Description |
+| -------- | ---- | ------- | ----------- |
+| `ctx` | `Context.<RequestParams.Update>` | **required** |  |
+
+### Results
+**Type:** any
+
+any
+
+
+## `delete` 
+
+DELETE
+
+### Parameters
+| Property | Type | Default | Description |
+| -------- | ---- | ------- | ----------- |
+| `ctx` | `Context.<RequestParams.Delete>` | **required** |  |
+
+### Results
+**Type:** any
+
+any
+
+
+## `count` 
+
+COUNT
+
+### Parameters
+| Property | Type | Default | Description |
+| -------- | ---- | ------- | ----------- |
+| `ctx` | `Context.<RequestParams.Count>` | **required** |  |
+
+### Results
+**Type:** any
+
+any
+
+
+## `search` 
+
+SEARCH
+
+### Parameters
+| Property | Type | Default | Description |
+| -------- | ---- | ------- | ----------- |
+| `ctx` | `Context.<RequestParams.Search>` | **required** |  |
+
+### Results
+**Type:** any
+
+any
+
+
+## `call` 
+
+CALL
+
+### Parameters
+| Property | Type | Default | Description |
+| -------- | ---- | ------- | ----------- |
+| `api` | `string` | **required** |  |
+| `params` | `*` | - |  |
+
+### Results
+**Type:** any
+
+any
+
+
 <!-- AUTO-CONTENT-END:ACTIONS -->
 
 <!-- AUTO-CONTENT-TEMPLATE:ACTIONS
--- @common/elastic/service.ts
+++ @common/elastic/service.ts
@@ -34,41 +34,114 @@ interface IServiceSettingSchema extends ServiceSettingSchema {
 class ElasticService extends Service<IServiceSettingSchema> {
   private client!: Client;
 
+  /**
+   * BULK
+   *
+   * @actions
+   *
+   * @param {Context<RequestParams.Bulk>} ctx
+   * @returns any
+   * @memberof ElasticService
+   */
   @action({ params: schema<RequestParams.Bulk>() })
   public async bulk(ctx: Context<RequestParams.Bulk>) {
     return this.client.bulk(ctx.params);
   }
 
+  /**
+   * CREATE
+   *
+   * @actions
+   *
+   * @param {Context<RequestParams.Create>} ctx
+   * @returns any
+   * @memberof ElasticService
+   */
   @action({ params: schema<RequestParams.Create>() })
   public async create(ctx: Context<RequestParams.Create>) {
     return this.client.create(ctx.params);
   }
 
+  /**
+   * GET
+   *
+   * @actions
+   *
+   * @param {Context<RequestParams.Get>} ctx
+   * @returns any
+   * @memberof ElasticService
+   */
   @action({ params: schema<RequestParams.Get>() })
   public async get(ctx: Context<RequestParams.Get>) {
     return this.client.get(ctx.params);
   }
 
+  /**
+   * UPDATE
+   *
+   * @actions
+   *
+   * @param {Context<RequestParams.Update>} ctx
+   * @returns any
+   * @memberof ElasticService
+   */
   @action({ params: schema<RequestParams.Update>() })
   public async update(ctx: Context<RequestParams.Update>) {
     return this.client.update(ctx.params);
   }
 
+  /**
+   * DELETE
+   *
+   * @actions
+   *
+   * @param {Context<RequestParams.Delete>} ctx
+   * @returns any
+   * @memberof ElasticService
+   */
   @action({ params: schema<RequestParams.Delete>() })
   public async delete(ctx: Context<RequestParams.Delete>) {
     return this.client.delete(ctx.params);
   }
 
+  /**
+   * COUNT
+   *
+   * @actions
+   *
+   * @param {Context<RequestParams.Count>} ctx
+   * @returns any
+   * @memberof ElasticService
+   */
   @action({ params: schema<RequestParams.Count>() })
   public async count(ctx: Context<RequestParams.Count>) {
     return this.client.count(ctx.params);
   }
 
+  /**
+   * SEARCH
+   *
+   * @actions
+   *
+   * @param {Context<RequestParams.Search>} ctx
+   * @returns any
+   * @memberof ElasticService
+   */
   @action({ params: schema<RequestParams.Search>() })
   public async search(ctx: Context<RequestParams.Search>) {
     return this.client.search(ctx.params);
   }
 
+  /**
+   * CALL
+   *
+   * @actions
+   *
+   * @param {string} api
+   * @param {*} [params]
+   * @returns any
+   * @memberof ElasticService
+   */
   @action()
   public async call(
     @param({ type: "string" }) api: string,
